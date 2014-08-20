806537b7b3f3b3e5ebd0c080e563464dfaa8c992
Jeremy List <quick.dudley@gmail.com>
2014-08-20 15:28:46 +0800

    Add isometry operations as well as isometries to push along axes.

--- Geometry.hs
+++ Geometry.hs
@@ -4,15 +4,25 @@ module Geometry () where
 import Data.Number.Fixed
 import Data.Ratio
 
-data Axis = H | X | Y | Z deriving (Ord,Enum,Show,Read)
+type Coord = Fixed Prec50
 
-data HyperPoint = HP (Fixed Prec50)
-  (Fixed Prec50) (Fixed Prec50) (Fixed Prec50) deriving Show
+data Axis = X | Y | Z deriving (Eq,Ord,Enum,Show,Read)
+
+data HyperPoint = HP Coord Coord Coord Coord deriving Show
 
 data HyperLine = HL HyperPoint HyperPoint deriving Show
 
-hyperPoint x y z = HP (sqrt (x^2 + y^2 + z^2 + 1)) x y z
+data Isometry = HI
+  Coord Coord Coord Coord
+  Coord Coord Coord Coord
+  Coord Coord Coord Coord
+  Coord Coord Coord Coord
 
+infixl 6 *>
+class Transformable t where
+  (*>) :: Isometry -> t -> t
+
+hyperPoint x y z = HP (sqrt (x^2 + y^2 + z^2 + 1)) x y z
 
 bilinear (HP h1 x1 y1 z1) (HP h2 x2 y2 z2) = h1*h2 - x1*x2 - y1*y2 - z1*z2
 
@@ -50,8 +60,86 @@ onLine (HL (HP h1 x1 y1 z1) (HP h2 x2 y2 z2)) (HP h x y z) = let
 axis a l = let
   x = sqrt ((cosh l)^2 - 1)
   in case a of
-    H -> hyperPoint 0 0 0
     X -> hyperPoint x 0 0
     Y -> hyperPoint 0 x 0
     Z -> hyperPoint 0 0 x
 
+instance Transformable HyperPoint where
+  (HI
+    hh hx hy hz
+    xh xx xy xz
+    yh yx yy yz
+    zh zx zy zz) *> (HP h x y z)= hyperPoint
+      (xh * h + xx * x + xy * y + xz * z)
+      (yh * h + yx * x + yy * y + yz * z)
+      (zh * h + zx * x + zy * y + zz * z)
+
+instance Transformable Isometry where
+  (HI
+    hh1 hx1 hy1 hz1
+    xh1 xx1 xy1 xz1
+    yh1 yx1 yy1 yz1
+    zh1 zx1 zy1 zz1) *> (HI
+     hh2 hx2 hy2 hz2
+     xh2 xx2 xy2 xz2
+     yh2 yx2 yy2 yz2
+     zh2 zx2 zy2 zz2) = HI {- I am sorry for this -}
+      (hh1*hh2 + hx1*xh2 + hy1*yh2 + hz1*zh2)
+      (hh1*hx2 + hx1*xx2 + hy1*yx2 + hz1*zx2)
+      (hh1*hy2 + hx1*xy2 + hy1*yy2 + hz1*zy2)
+      (hh1*hz2 + hx1*xz2 + hy1*yz2 + hz1*zz2)
+      (xh1*hh2 + xx1*xh2 + xy1*yh2 + xz1*zh2)
+      (xh1*hx2 + xx1*xx2 + xy1*yx2 + xz1*zx2)
+      (xh1*hy2 + xx1*xy2 + xy1*yy2 + xz1*zy2)
+      (xh1*hz2 + xx1*xz2 + xy1*yz2 + xz1*zz2)
+      (yh1*hh2 + yx1*xh2 + yy1*yh2 + yz1*zh2)
+      (yh1*hx2 + yx1*xx2 + yy1*yx2 + yz1*zx2)
+      (yh1*hy2 + yx1*xy2 + yy1*yy2 + yz1*zy2)
+      (yh1*hz2 + yx1*xz2 + yy1*yz2 + yz1*zz2)
+      (zh1*hh2 + zx1*xh2 + zy1*yh2 + zz1*zh2)
+      (zh1*hx2 + zx1*xx2 + zy1*yx2 + zz1*zx2)
+      (zh1*hy2 + zx1*xy2 + zy1*yy2 + zz1*zy2)
+      (zh1*hz2 + zx1*xz2 + zy1*yz2 + zz1*zz2)
+
+identity = HI
+  1 0 0 0
+  0 1 0 0
+  0 0 1 0
+  0 0 0 1
+
+pushx alpha = let
+  c = cosh alpha
+  s = sinh alpha
+  in HI
+   c s 0 0
+   s c 0 0
+   0 0 1 0
+   0 0 0 1
+
+pushy alpha = let
+  c = cosh alpha
+  s = sinh alpha
+  in HI
+   c 0 s 0
+   0 1 0 0
+   s 0 c 0
+   0 0 0 1
+
+pushz alpha = let
+  c = cosh alpha
+  s = sinh alpha
+  in HI
+   c 0 0 s
+   0 1 0 0
+   0 0 1 0
+   s 0 0 c
+
+push d = case d of
+  X -> pushx
+  Y -> pushy
+  Z -> pushz
+{-
+  +c 0 +s
+  0  1  0
+  +s 0 +c
+-}
